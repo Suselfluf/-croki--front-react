3c2d04bbc266a73bad5e3b7cc170dcd7ec9a2e52
Suselfluf <danya.lyagin@gmail.com>
2024-06-29 18:46:14 +0300

    url fixes

--- src/const/ulrs.js
+++ src/const/ulrs.js
@@ -1,8 +1,7 @@
 
-export const MESHROOM_URL = "http://192.168.100.28:5000/";
-export const SEND_IMAGES_MESHROOM_URL = "http://192.168.100.28:5000//bulkUpload"; // PC
+export const MESHROOM_URL = "http://localhost:5000/";
+export const SEND_IMAGES_MESHROOM_URL = "http://localhost:5000//bulkUpload"; // PC
 export const BACKEND_CLOTHES_URL =
-  "http://192.168.100.31:8000/api/clothes-admin";
-export const BACKEND_URL = "http://192.168.100.31:8000";
+  "http://127.0.0.1:8000/api/clothes-admin";
+export const BACKEND_URL = "http://127.0.0.1:8000/";
 export const BACKEND_MEDIA_URL = BACKEND_URL + "/media";
-
